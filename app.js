ae4a632ef4f67ec1b29c7db0b53132f48fba3bf6
Heriniaina Ramamonjitiana <heriniainaramamonjitiana@gmail.com>
2025-07-09 16:34:26 +0300

    fir

---
+++ app.js
@@ -0,0 +1,40 @@
+import express from "express";
+import cors from "cors";
+import { Server } from "socket.io";
+import http from "http";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+const server = http.createServer(app);
+
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+
+io.on("connection", (socket) => {
+  console.log("WebSocket connecte :", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("WebSocket déconnecté :", socket.id);
+  });
+});
+
+app.post("/test", (req, res) => {
+  console.log(" Requête Rn reçue");
+  io.emit("test");
+  res.send("ok");
+});
+
+
+const PORT = 8000;
+server.listen(PORT, () => {
+  console.log(`Serveur: http://192.168.42.1:${PORT}`);
+});
